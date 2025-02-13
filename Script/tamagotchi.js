6480a302208ae6e25f1184c529e584028d8c4fa0
Sovichet-Rathanak <rathanaksovichet@gmail.com>
2025-02-13 18:48:25 +0700

    detailing tamagotchi

--- Script/tamagotchi.js
+++ Script/tamagotchi.js
@@ -4,6 +4,7 @@ const eatBtn = document.querySelector(".button-eat");
 const walkBtn = document.querySelector(".button-walk");
 const bathroomBtn = document.querySelector(".button-bathroom");
 const livingroomBtn = document.querySelector(".button-living");
+const buttonArray = [sleepBtn, eatBtn, walkBtn, bathroomBtn, livingroomBtn];
 //Scenes and Screen
 const bedroom = "/Assets/Tamagotchi/bedroom.png";
 const kitchen = "/Assets/Tamagotchi/food.jpg";
@@ -13,34 +14,51 @@ const livingroom = "/Assets/Tamagotchi/livingRoom.jpg";
 const screenScene = document.querySelector(".scene");
 const loadingScreen = document.querySelector(".loading-screen");
 //Stats
-let sleepiness = 100;
-let hunger = 100;
-let boredom = 100;
-let cleanliness = 100;
-const statsArray = [sleepiness, hunger, boredom, cleanliness]; //I think we need to change this into an array of object instead
-let gameRunning = false;
-
+const stats = [
+    {
+        state: "Sleepiness",
+        currentStat: 100,
+    },
+    {
+        state: "Hunger",
+        currentStat: 100,
+    },    {
+        state: "Boredom",
+        currentStat: 100,
+    },    {
+        state: "Cleanliness",
+        currentStat: 100,
+    },
+]; 
+let gameRunning = true;
+//Miscellaneous
+const buttonSfx = new Audio("/Assets/Tamagotchi/button.mp3");
 window.onload = firstLoad();
 
+function actionHandler(index, scene){
+    buttonSfx.play();
+    loadScene(scene);
+    if(stats[index]["currentStat"] < 100){
+        stats[index]["currentStat"] += Math.floor(Math.random() * (3-1) + 1);
+    }
+    updateUI();
+}
+
 sleepBtn.addEventListener('click', function(){
-    loadScene(bedroom);
+    actionHandler(0, bedroom);
 })
 
 eatBtn.addEventListener('click', function(){
-    loadScene(kitchen);
+    actionHandler(1, kitchen);
 })
 
 walkBtn.addEventListener('click', function(){
-    loadScene(outside);
+    actionHandler(2, outside);
 })
 
 bathroomBtn.addEventListener('click', function(){
-    loadScene(bathroom);
-    let randNum = Math.ceil(Math.random() * statsArray.length - 1);
-    statsArray[randNum] -= 1;
-    console.log(statsArray);
+    actionHandler(3, bathroom);
 })
-
 
 function loadScene(scene){
     loadingScreen.style.display = "flex";
@@ -48,26 +66,39 @@ function loadScene(scene){
         loadingScreen.style.display = "none";
         screenScene.src = scene;
     }, 500);
+
+    if(scene && scene != livingroom){
+        setTimeout(function() {
+            screenScene.src = livingroom;
+        }, 3000);
+    }
 }
 
 function firstLoad(){
     setTimeout(function(){
         loadingScreen.style.display = "none";
         loadScene(livingroom);
+        randomiser();
     }, 1000)
 }
 
 function randomiser() {
-    while(gameRunning){
-        let randNum = Math.ceil((Math.random() * statsArray.length - 1));
-        statsArray[randNum] -= 1;
-    }
+    if(!gameRunning) return;
+    
+    let randNum = Math.floor((Math.random() * stats.length));
+    stats[randNum]["currentStat"] -= Math.ceil(Math.random() * (5 - 1) + 1);
+
+    updateUI();
+
+    let randomTick = Math.random() * (4000 - 1000) + 1000;
+    setTimeout(randomiser, randomTick);
 }
 
-randomiser();
+function updateUI() {
+    document.querySelector(".sleep-percent").textContent = `${stats[0]["currentStat"]}%`;
+    document.querySelector(".hunger-percent").textContent = `${stats[1]["currentStat"]}%`;
+    document.querySelector(".boredom-percent").textContent = `${stats[2]["currentStat"]}%`;
+    document.querySelector(".clean-percent").textContent = `${stats[3]["currentStat"]}%`;
+}
 
-//TODO: Add SFX
-//TODO: Randomly decrease the stat percentage
-//TODO: Return to living room after completing the task
-//TODO: Increment stat after completing the task
 //TODO: Game over function
